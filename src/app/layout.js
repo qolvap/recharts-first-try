246701ce43ce1a9e959046d644b1223e0cf83806
qolvap <pgrebennikow@yahoo.pl>
2024-05-17 23:05:40 +0200

    code

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="bg-slate-950 text-white">
+    <html lang="en" className="bg-slate-300 text-white">
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -24,8 +24,8 @@ export default function Home() {
 
 function GridItem({ title, children }) {
   return (
-    <div className="flex flex-col items-center justify-center p-4 border border-slate-900 bg-slate-900/50 rounded-xl h-[400px]">
-      <h3 className="text-2xl font-semibold text-white mb-4">{title}</h3>
+    <div className="flex flex-col items-center justify-center p-4 border border-slate-900 bg-slate-50 rounded-xl h-[400px]">
+      <h3 className="text-2xl font-semibold text-black mb-4">{title}</h3>
       {children}
     </div>
   );
